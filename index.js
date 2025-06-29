7db83d94410dd3685e977660fdba2c171beb3aab
Ramandeepbunny <malkit.k31991@gmail.com>
2025-06-28 17:22:28 -0700

    time and emoji changed

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+
+
+
+
+
+function formatDate(date) { 
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[date.getDay()];  
+  let minutes = date.getMinutes();
+  let hour = date.getHours();
+  
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hour}:${minutes}`;
+  
+}
+
+
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temp");
   let temperature = response.data.temperature.current;
@@ -5,7 +26,10 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#Wind");
   let descriptionElement = document.querySelector("#description");
   let iconElement = document.querySelector("#emoji");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
   
+  timeElement.innerHTML = formatDate(date);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="emoji" />`;
   descriptionElement.innerHTML = `${response.data.condition.description}`;
   windElement.innerHTML = `${response.data.wind.speed}`;
@@ -31,3 +55,7 @@ function SearchSubmit(event) {
 }
 let searchFormElement =  document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", SearchSubmit);
+
+function backgroundColor(color) {
+  if formatDate(date) =
+}
